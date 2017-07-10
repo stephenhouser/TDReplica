17305e7e57733546ccab59f086f30a2d85be64e9
Stephen Houser <stephenhouser@gmail.com>
2017-07-10 16:07:45 -0400

    merge bbpress into tdreplica.css

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ function my_theme_enqueue_styles() {
     $parent_style = 'twentyfourteen-style'; 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
+        get_stylesheet_directory_uri() . '/tdreplica.css',
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
