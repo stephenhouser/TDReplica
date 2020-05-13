46359eac4f58355f6d58f3ba8d706039b37937a3
Stephen Houser <stephenhouser@gmail.com>
2020-05-13 10:06:34 -0400

    rename some functions and working security code

--- functions.php
+++ functions.php
@@ -7,47 +7,44 @@
 /* 
  * Add local, TDReplica style.css to list of used style sheets. 
  */
-function theme_enqueue_styles() {
+add_action( 'wp_enqueue_scripts', 'tdr_enqueue_styles' );
+function tdr_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
 }
-
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
 /* 
  * Only show the admin bar when logged in
  */
 add_filter( 'show_admin_bar', '__return_true' , 1000 );
 
-function mytheme_admin_bar_render() {
+add_action( 'wp_before_admin_bar_render', 'tdr_admin_bar_render' );
+function tdr_admin_bar_render() {
     global $wp_admin_bar;
     if ( !is_user_logged_in() ) {
         global $wp_admin_bar;
     }
 }        
 
-add_action( 'wp_before_admin_bar_render', 'mytheme_admin_bar_render' );
-
 /* 
  * Add MG Safety Fast logo to login screen 
  */
-function my_login_logo() { 
-?>
-    <style type="text/css">
-        .login h1 a {
-            background-image: url(/wp-content/uploads/MGSafetyFast.png) !important;
-            padding-bottom: 30px;
-        }
-    </style>
-<?php 
+add_action( 'login_enqueue_scripts', 'tdr_login_logo' );
+function tdr_login_logo() { 
+	?>
+		<style type="text/css">
+			.login h1 a {
+				background-image: url(/wp-content/uploads/MGSafetyFast.png) !important;
+				padding-bottom: 30px;
+			}
+		</style>
+	<?php 
 }
-
-add_action( 'login_enqueue_scripts', 'my_login_logo' );
 
 /*
  * Add @mentionname after bbpress forum author details
  */
-function mentionname_to_bbpress () {
+add_action( 'bbp_theme_after_reply_author_details', 'tdr_mentionname_to_bbpress' );
+function tdr_mentionname_to_bbpress () {
     $user = get_userdata( bbp_get_reply_author_id() );
     if ( !empty( $user->user_nicename ) ) {
         $user_nicename = $user->user_nicename;
@@ -59,49 +56,44 @@ function mentionname_to_bbpress () {
     }
 }
 
-add_action( 'bbp_theme_after_reply_author_details', 'mentionname_to_bbpress' );
 
 /* 
  * Change FROM address and name for outgoing email 
  */
-function new_mail_from($old) {
+add_filter('wp_mail_from', 'tdr_new_mail_from');
+function tdr_new_mail_from($old) {
 	return 'webmaster@tdreplica.com';
 }
-function new_mail_from_name($old) {
+
+add_filter('wp_mail_from_name', 'tdr_new_mail_from_name');
+function tdr_new_mail_from_name($old) {
 	return 'TDReplica.com Wordpress';
 }
 
-add_filter('wp_mail_from', 'new_mail_from');
-add_filter('wp_mail_from_name', 'new_mail_from_name');
 
 /*
  * Add user id to users list
  */
-add_filter('manage_users_columns', 'pippin_add_user_id_column');
-function pippin_add_user_id_column($columns) {
+add_filter('manage_users_columns', 'tdr_add_user_id_column');
+function tdr_add_user_id_column($columns) {
     $columns['user_id'] = 'User ID';
     return $columns;
 }
 
-add_action('manage_users_custom_column',  'pippin_show_user_id_column_content', 10, 3);
-function pippin_show_user_id_column_content($value, $column_name, $user_id) {
+add_action('manage_users_custom_column',  'tdr_show_user_id_column_content', 10, 3);
+function tdr_show_user_id_column_content($value, $column_name, $user_id) {
     $user = get_userdata( $user_id );
         if ( 'user_id' == $column_name )
                 return $user_id;
     return $value;
 }
 
-add_action('signup_extra_fields', 'my_register_form');
-function my_register_form() {
-	?><p> THIS IS IT </p><?php
-}
-
 /*
  * Include any content from Register page onto the register page
  * ...I know, seems obvious but buddypress does not show it!
  */
-add_action('bp_before_account_details_fields', 'registration_text');
-function registration_text() {
+add_action('bp_before_account_details_fields', 'tdr_registration_text');
+function tdr_registration_text() {
 	$page = get_page_by_title('Register');
 	$content = apply_filters('the_content', $page->post_content);
 	echo $content;
@@ -109,30 +101,36 @@ function registration_text() {
 
 add_action('bp_account_details_fields', 'registration_invitation', 10);
 function registration_invitation() {
-	$invitation = (!empty($_POST['invitation'])) ? sanitize_text_field($_POST['invitation'] ) : '';
+	$invitation = (!empty($_POST['signup_invitation'])) ? sanitize_text_field($_POST['signup_invitation'] ) : '';
 	?>
-	<label for="invitation"><?php _e('Invitation Code', 'registration_invitation') ?> <?php _e( '(required)', 'registration_invitation' ); ?>
-	<input type="text" name="invitation" id="invitation" class="input" value="<?php echo esc_attr($invitation); ?>" size="25" /></label>
+		<label for="signup_invitation">
+			<?php _e('Invitation Code', 'registration_invitation') ?> 
+			<?php _e( '(required)', 'registration_invitation' ); ?>
+		</label>		
+		<?php do_action('bp_signup_invitation_errors'); ?>
+
+		<input type="text" name="signup_invitation" id="signup_invitation" 
+				class="input" value="<?php echo esc_attr($invitation); ?>" size="25" />
 	<?php
 }
 
-/*
 add_action('bp_signup_validate', 'registration_invitation_validate');
 function registration_invitation_validate() {
 	$bp = buddypress();
-	if (empty($_POST['invitation']) || (!empty($_POST['invitation']) && trim($_POST['invitation']) != 'tdreplica')) {
-		$bp->signup->errors['invitation'] = __('You must include the registration invitation code.', 'buddypress');
+	if (empty($_POST['signup_invitation']) || (!empty($_POST['signup_invitation']) && trim($_POST['signup_invitation']) != 'tdreplica')) {
+    	$bp->signup->errors['signup_invitation'] = __('You must include a correct invitation code.', 'registration_invitation');
 	}
 }
- */
 
+/*
 add_filter('bp_core_validate_user_signup', 'registration_invitation_validation');
 function registration_invitation_validation($result) {
-	if (empty($_POST['invitation']) || (!empty($_POST['invitation']) && trim($_POST['invitation']) != 'tdreplica')) {
-		$result['errors']->add('invitation', __('You must include the correct registration invitation code.', 'registration_invitation'));
+	if (empty($_POST['signup_invitation']) || (!empty($_POST['signup_invitation']) && trim($_POST['signup_invitation']) != 'tdreplica')) {
+		// $result['errors']->add('signup_invitation', __('You must include the correct registration invitation code.', 'registration_invitation'));
 		$result['errors']->add('user_name', __( 'You must include the correct registration invitation code!', 'buddypress'));
 	}
 
 	return $result;
 }
+*/
 ?>
