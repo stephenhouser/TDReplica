8f6a19f62c7cebe3004619367ece00c6a5b719a3
Stephen Houser <stephenhouser@gmail.com>
2020-05-12 21:50:48 -0400

    make work better with buddypress and add security check

--- functions.php
+++ functions.php
@@ -90,4 +90,55 @@ function pippin_show_user_id_column_content($value, $column_name, $user_id) {
                 return $user_id;
     return $value;
 }
+
+add_action('signup_extra_fields', 'my_register_form');
+function my_register_form() {
+	?><p> THIS IS IT </p><?php
+}
+
+/*
+ * Add text to the top of the user registration page
+ */
+add_action('bp_before_account_details_fields', 'registration_text');
+function registration_text() {
+	?>
+	<p>Because our primary goal is the promotion of our hobby and the enjoyment of these great cars, we DO NOT collect dues or have bunch of rules to follow. To become a member, we only have three simple requests:</p>
+	<ol>
+		<li><strong>Help us foster an active, supportive, and family friendly environment. Always treat everyone with dignity and respect.</strong> We are not in competition for prizes or glory.</li>
+		<li>If someone gives you useful advice or help, return the favor and help someone else. If not for the support and advice of others, many of these cars would never see the road. SHARE, HELP, AND SUPPORT. Where past kit car manufacturers lacked support and nearly destroyed the hobby, we fill in that gap and ensure others get to enjoy the full benefit of owning a fun little car.</li>
+		<li>HAVE FUN. We’ve received story after story about heart break and disappointment when a project doesn’t seem to work out as planned. Remain vigilant in the idea that this all about having fun and not for profit.</li>
+	</ol>
+
+	<p>All proceeds from items sold in our online shop go directly to the club fund to ensure our continued growth. Funds are used to purchase items in quantity so we can offer them at a lower price to our members. Items like Grill Badges, T-Shirts and Hats have a very high up front cost. Website hosting is a relatively inexpensive cost and all development and maintenance is done strictly on members’ own time with no compensation.</p>
+	<?php
+}
+
+add_action('bp_account_details_fields', 'registration_invitation', 10);
+function registration_invitation() {
+	$invitation = (!empty($_POST['invitation'])) ? sanitize_text_field($_POST['invitation'] ) : '';
+	?>
+	<label for="invitation"><?php _e('Invitation Code (required)', 'registration_invitation') ?> <?php _e( '(required)', 'registration_invitation' ); ?>
+	<input type="text" name="invitation" id="invitation" class="input" value="<?php echo esc_attr($invitation); ?>" size="25" /></label>
+	<?php
+}
+
+/*
+add_action('bp_signup_validate', 'registration_invitation_validate');
+function registration_invitation_validate() {
+	$bp = buddypress();
+	if (empty($_POST['invitation']) || (!empty($_POST['invitation']) && trim($_POST['invitation']) != 'tdreplica')) {
+		$bp->signup->errors['invitation'] = __('You must include the registration invitation code.', 'buddypress');
+	}
+}
+ */
+
+add_filter('bp_core_validate_user_signup', 'registration_invitation_validation');
+function registration_invitation_validation($result) {
+	if (empty($_POST['invitation']) || (!empty($_POST['invitation']) && trim($_POST['invitation']) != 'tdreplica')) {
+		$result['errors']->add('invitation', __('You must include the correct registration invitation code.', 'registration_invitation'));
+		$result['errors']->add('user_name', __( 'You must include the correct registration invitation code!', 'buddypress'));
+	}
+
+	return $result;
+}
 ?>
